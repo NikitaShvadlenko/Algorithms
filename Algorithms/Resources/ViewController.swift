2611b8ebc0c177a50978e4cfe3bcf9ddfc210d3a
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2023-01-17 21:05:33 +1300

    Initial commit

---
+++ Algorithms/Resources/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Algorithms
+//
+//  Created by Nikita Shvad on 17.01.2023.
+//  Copyright © 2023 Nikita Shvadlenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+    }
+}
